306217f4dfdd6a8f71df600c260bdb47a9987023
Michal Jaworowski <kzpl_kropeq@interia.pl>
2017-08-09 18:10:33 +0200

    Stworzenie prefab królika, replikowanie go, ustalenie zasad poruszania się

---
+++ Unity 5/BunnyMark/Assets/Scripts/BunnyMark.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+// required to UI elements
+using UnityEngine.UI;
+using System.Collections;
+
+public class BunnyMark : MonoBehaviour {
+
+	GameObject rabbit;
+	Text text;
+
+	// storing the result value
+	int result = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+		// initialize prepared prefab of rabbit sprite
+		rabbit = Resources.Load<GameObject> ("Prefabs/Rabbit");
+
+		// initialize Text component to update the text later
+		text = GameObject.Find ("Result").GetComponent<Text> ();
+	}
+
+	// it is executed when user click in the canvas area
+	// add 25 moving rabbits to the scene
+	public void addRabbits(){
+		
+		// update the result
+		result += 25;
+
+		// create 25 rabbits from the "rabbit" prefab
+		for (int i = 0; i < 25; i++) {
+			Instantiate (rabbit);
+		}
+
+		// update text of result on the GUI
+		text.text = "Rabbits: " + result;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity 5/BunnyMark/Assets/Scripts/Moves.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moves : MonoBehaviour {
+
+	// direction of moving: 1 = down, -1 = up
+	int direction = 1;
+
+	// Update is called once per frame
+	void Update () {
+		// update position of this rabbit
+		this.transform.position = new Vector2 (this.transform.position.x, this.transform.position.y - 0.07f * direction);
+
+		// if rabbit is going out of the screen, change direction of moving
+		if (this.transform.position.y < -4.85f) {
+			direction = -1;
+			this.transform.position = new Vector2(this.transform.position.x, -4.85f);
+		}
+
+		// if rabbit is going out of the screen, change direction of moving
+		if (this.transform.position.y > 4.85f) {
+			direction = 1;
+			this.transform.position = new Vector2(this.transform.position.x, 4.85f);
+		}
+	}
+}

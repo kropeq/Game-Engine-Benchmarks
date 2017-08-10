f9952011be2aee333ba78b1a21af7c90a2b53024
Michal Jaworowski <kzpl_kropeq@interia.pl>
2017-08-10 09:27:25 +0200

    Zmiana na listę struktur w celu optymalizacji wyników

--- Unity 5/BunnyMark/Assets/Scripts/BunnyMark.cs
+++ Unity 5/BunnyMark/Assets/Scripts/BunnyMark.cs
@@ -2,9 +2,35 @@
 // required to UI elements
 using UnityEngine.UI;
 using System.Collections;
+// required to List
+using System.Collections.Generic;
+
+struct RabbitObj {
+	// direction of moving: 1 = down, -1 = up
+	private int direction;
+	private GameObject rabbit;
+	// change direction of moving
+	public void SetDirection(int _value){
+		direction = _value;
+	}
+	// setting sprite
+	public void SetRabbit(GameObject _value){
+		rabbit = _value;
+	}
+	public int GetDirection(){
+		return direction;
+	}
+	public GameObject GetRabbit(){
+		return rabbit;
+	}
+}
 
 public class BunnyMark : MonoBehaviour {
 
+	// constant size: half height of the board
+	// it's constant cause it's set to Fit Height for multiresolutions
+	const float size = 4.85f;
+	// declarations
 	GameObject rabbit;
 	Text text;
 	// storing the result value
@@ -12,7 +38,13 @@ public class BunnyMark : MonoBehaviour {
 	// storing the time difference between frames
 	float deltaTime = 0.0f;
 	// value of increasing
-	int increase = 100;
+	int increase = 50;
+	// this stores clone of rabbit GameObject
+	GameObject newRabbit;
+	// screen size values
+	float height,width,ratio;
+	// List that stores all rabbits
+	IList<RabbitObj> rabbits = new List<RabbitObj>();
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +52,14 @@ public class BunnyMark : MonoBehaviour {
 		rabbit = Resources.Load<GameObject> ("Prefabs/Rabbit");
 		// initialize Text component to update the text later
 		text = GameObject.Find ("Result").GetComponent<Text> ();
+		// get Screen height and width
+		height = Screen.height;
+		width = Screen.width;
+		// count ratio of width and height
+		ratio = width / height;
+		// multiply ratio by height of the board
+		// width range: <-ratio,ratio>
+		ratio = ratio * size;
 	}
 
 	// it is executed when user click in the canvas area
@@ -27,7 +67,7 @@ public class BunnyMark : MonoBehaviour {
 	public void addRabbits(){
 		// if computer: add 100, if smartphone: add 10
 		#if UNITY_STANDALONE
-			increase = 100;
+			increase = 50;
 		#elif UNITY_ANDROID
 			increase = 10;
 		#endif
@@ -35,7 +75,16 @@ public class BunnyMark : MonoBehaviour {
 		result += increase;
 		// create the next rabbits from the "rabbit" prefab
 		for (int i = 0; i < increase; i++) {
-			Instantiate (rabbit);
+			// clone the rabbit prefab
+			newRabbit = Instantiate (rabbit);
+			// set random position of the prefab width in range <-ratio,ratio>
+			newRabbit.transform.position = new Vector2(Random.Range(-ratio,ratio),Random.Range(-size,size));
+			// create new struct object
+			RabbitObj ro = new RabbitObj ();
+			ro.SetRabbit (newRabbit);
+			ro.SetDirection (1);
+			// add this struct to list
+			rabbits.Add (ro);
 		}
 		// update text of result on the GUI
 		text.text = "Rabbits: " + result;
@@ -44,6 +93,31 @@ public class BunnyMark : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		// loop through all of the rabbits in the list
+		for (int i = 0; i < rabbits.Count; i++) {
+			// get properties of this sprite
+			GameObject temp = rabbits [i].GetRabbit ();
+			int direction = rabbits [i].GetDirection ();
+			// update the position of this sprite
+			temp.transform.position = new Vector2(temp.transform.position.x, temp.transform.position.y - 0.07f * (rabbits[i].GetDirection()));
+			// change direction of moving if it goes too far
+			if (temp.transform.position.y < -size) {
+				direction = -1;
+				temp.transform.position = new Vector2 (temp.transform.position.x, -size);
+			}
+			if (temp.transform.position.y > size) {
+				direction = 1;
+				temp.transform.position = new Vector2 (temp.transform.position.x, size);
+			}
+			// create new struct object
+			RabbitObj ro = new RabbitObj ();
+			ro.SetRabbit (temp);
+			ro.SetDirection (direction);
+			// you must to replace all structure, not just variable
+			// because it changes only a copy of structure
+			// not the original one
+			rabbits [i] = ro;
+		}
 	}
 
 	// drawing on GUI every frame and executed as the last method
